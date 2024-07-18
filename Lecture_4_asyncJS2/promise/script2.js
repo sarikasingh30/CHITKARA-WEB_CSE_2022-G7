57061e02d53da514b9119713a7decdd0445ea4ce
sarika <sarika.goooogle.1330@gmail.com>
2024-07-18 21:26:56 +0530

    updated lecture_4

--- Lecture_4_asyncJS2/promise/script2.js
+++ Lecture_4_asyncJS2/promise/script2.js
@@ -6,12 +6,6 @@
 
 
 
-
-
-
-
-
-
 function downloadMovie (url) {
     return new Promise(function (resolve, reject) {
         let ext = url.split('/').pop().split('.').pop(); 
@@ -33,3 +27,61 @@ console.log(movie);
 }).catch(function (err) {
 console.log(err);
 })
+
+
+// ....................................................
+// download => compress => upload (PROMISE CHAINING)
+// ....................................................
+function downloadMovie (url) {
+    return new Promise(function (resolve, reject) {
+        let ext = url.split('/').pop().split('.').pop(); 
+        if(ext != 'mp4'){
+            reject("Sahi URL dedo");
+        }
+        else {
+            setTimeout(function(){
+            resolve(url.split('/').pop());
+            }, 2000);
+        }
+    })
+}
+// downloadMovie function ek promise return krta hai, 
+// downloadMovie is a promise 
+
+function compressMovie (file) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function(){
+            let compressedM = file.split('.')[0]+".zip"; 
+            console.log("filecompressed",compressedM)
+            resolve(compressedM);
+        }, 2000);
+        
+    })
+}
+
+function uploadMovie (file) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function(){
+            let newLink= "www.newSite.com/"+file; 
+            console.log("file uploaded",newLink)
+            resolve(newLink);
+        }, 2000);
+        
+    })
+}
+
+downloadMovie('myurl.com/avengers.mp4')
+.then(function(movie){
+console.log(movie);
+return movie
+}).then(compressMovie)
+.then(uploadMovie)
+.catch(function (err) {
+console.log(err);
+})
+
+
+
+
+
+
