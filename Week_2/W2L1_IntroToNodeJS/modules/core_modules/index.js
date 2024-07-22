a973bc01fdba3c27834bdc6ec3fd3e55046df68e
sarika <sarika.goooogle.1330@gmail.com>
2024-07-22 21:50:07 +0530

    W2L1 updated 1

---
+++ Week_2/W2L1_IntroToNodeJS/modules/core_modules/index.js
@@ -0,0 +1,10 @@
+
+// os module
+const os=require("os")
+console.log(os.type())
+
+
+// path module
+const path=require("path")
+console.log(path.dirname("C:\Users\\1223\Desktop\G-7Lecture1\CSE_2022-G7\Week_2\W2L1_IntroToNodeJS\modules\core_modules\index.js"))
+console.log(path.extname("C:\Users\\1223\Desktop\G-7Lecture1\CSE_2022-G7\Week_2\W2L1_IntroToNodeJS\modules\core_modules\index.js"))
