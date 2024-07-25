fc7d94aa10ec8ddfc205dccac7bce8525d659222
sarika <sarika.goooogle.1330@gmail.com>
2024-07-25 23:53:59 +0530

    updated W2L4

--- Week_2/W2L3_fs/fileHandling/task/index.js
+++ Week_2/W2L3_fs/fileHandling/task/index.js
@@ -1,3 +1,6 @@
+// # Way 1
+const fs=require("fs")
+const path=require("path")
 function generateRandomNumbers() {
     const randomNumbers = [];
     for (let i = 0; i < 50; i++) {
@@ -7,28 +10,124 @@ function generateRandomNumbers() {
     
     return randomNumbers;
 }
-const fs=require("fs")
-const path=require("path")
+
 filePath=path.join(__dirname,"data1.json")
 filePath2=path.join(__dirname,"data2.json")
 
 let ans1= generateRandomNumbers();
 let ans2= generateRandomNumbers();
-console.log(ans1,ans2);
-fs.writeFile(filePath,JSON.stringify(ans1),(err)=>{
-    if(err){
-        console.log(err)
-    }
-    else{
-        console.log("kkkkDone")
-    }
-})
 
-fs.writeFile(filePath2,JSON.stringify(ans2),(err)=>{
-    if(err){
-        console.log(err)
-    }
-    else{
-        console.log("kkkkDone")
-    }
-})
+//write
+fs.writeFileSync(filePath,JSON.stringify(ans1))
+fs.writeFileSync(filePath2,JSON.stringify(ans2))
+
+// read
+let data1=JSON.parse(fs.readFileSync(filePath,"utf-8"))
+let data2=JSON.parse(fs.readFileSync(filePath2,"utf-8"))
+function mergeAndSort(data1,data2){
+    let data=[...data1, ...data2]
+    data.sort((a,b)=>a-b)
+    return data
+}
+let output=mergeAndSort(data1,data2)
+filePath3=path.join(__dirname,"output.json")
+
+fs.writeFileSync(filePath3,JSON.stringify(output))
+
+// ......................................................
+
+
+
+
+
+
+
+// ..................................................
+// # Way 2
+// const fs = require('fs/promises')
+// const path = require('path')
+
+// let path1 = path.join(__dirname,'/file1.txt')
+// let path2 = path.join(__dirname,'/file2.txt')
+// let path3 = path.join(__dirname,'/file3.txt')
+
+// let arr1 = []
+// for(let i=1;i<=50;i++){
+//     arr1.push(i)
+// }
+// let arr2 = []
+// for(let i=11;i<=60;i++){
+//     arr2.push(i);
+// }
+
+// async function writeAndMerge(){
+//     try{
+//         fs.writeFile(path1,arr1.toString());
+//         fs.writeFile(path2,arr2.toString());
+
+//         const data1 = await fs.readFile(path1,'utf8')
+//         const data2 = await fs.readFile(path2,'utf8')
+//         console.log(typeof(data1), typeof(data2))
+//         const array1 = data1.split(',').map(Number)
+//         const array2 = data2.split(',').map(Number)
+//         console.log(array1, array2)
+
+//         const mergeArray = [...array1,...array2]
+//         console.log(mergeArray)
+
+//         mergeArray.sort((a,b) => a-b)
+//         console.log(mergeArray)
+
+//         fs.writeFile(path3,mergeArray.toString(),(err)=>{
+//             if(err){
+//                 console.log(err)
+//             }
+//             else{
+//                 console.log("written")
+//             }
+//         })
+       
+//     }   catch(error){
+//         console.log('failed',error)
+//     }
+    
+// }
+// writeAndMerge()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Week_2/W2L4_fs(continue)+project/mini_project/index.js
@@ -0,0 +1,18 @@
+// Project Discussion (MINI PROJECT => IMAGE COMPRESS APP) 
+// base64 
+
+/*
+• Base64 encoding is a way to convert binary data into a 
+text string composed of 64 different ASCII characters.
+• It is commonly used for encoding data for text-based
+ communication protocols.
+• The encoded data can be safely transmitted over media 
+that are designed to deal with text.
+
+Why ?
+Binary data can be corrupted when transmitted over systems
+ that are not 8-bit clean or where binary data can be
+  misinterpreted. Base64 encoding ensures that the data 
+  remains intact during transit by converting it to a 
+  standard text format.
+*/
