1963926f14f81b85d8abfd6728ac8cf4c51121fb
sarika <sarika.goooogle.1330@gmail.com>
2024-08-03 00:16:40 +0530

    W3L5 updated

---
+++ Week_3/W3L5_TodoApp/TodoApp/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.json()); // otherwise Axios request could not be read
+app.use(express.static(path.join(__dirname, "public")));
+
+let todos = [
+  { id: "1", name: "Task-1", isCompleted:true },
+  { id: "2", name: "Task-2", isCompleted:false },
+];
+app.get("/gettodos", (req, res) => {
+  res.send(todos);
+});
+
+app.post("/addtodos", (req, res) => {
+  // const name=req.body.name
+  const { name, isCompleted } = req.body;
+  val=isCompleted=="true"?true:false
+  todos.push({
+    id: uuidv4(),
+    name,
+    isCompleted:val
+  });
+  res.redirect("/gettodos")
+});
+
+app.post("/deletetodos", (req, res) => {
+  const {id}=req.body
+  todos=todos.filter((el)=>{
+        if(el.id==id) return false
+        return true
+  })
+  res.redirect("/gettodos")
+
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("error", err);
+  } else {
+    console.log(`Listening on Port ${PORT}`);
+  }
+});
---
+++ Week_3/W3L5_TodoApp/TodoApp/public/script.js
@@ -0,0 +1,3 @@
+const btn = document.querySelector('.btn');
+const newtask = document.querySelector('.newtask');
+const taskList = document.querySelector('.tasklist');
